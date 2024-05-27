43f84ea720562110313123ab38cf03e726452dd3
Dipak Gupta <dipakg286@gmail.com>
2024-05-27 20:46:27 +0530

    first commit

---
+++ Trie.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("../input.txt", "r", stdin);
+	freopen("../output.txt", "w", stdout);
+#endif
+	int n;
+	cin >> n;
+	cout << "Hello World" << n << endl;
+	if (true) {
+		cout << "Here" << endl;
+	}
+	else {
+		cout << "Here" << endl;
+	}
+	return 0;
+}
